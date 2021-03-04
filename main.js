ae89e963140d003cccf2a0d39fdc97ebed7d1c78
wvwall <waltervelardo@gmail.com>
2021-03-04 15:23:07 +0100

    Ordinati, problema seconda parte

---
+++ main.js
@@ -0,0 +1,25 @@
+// Creare array cognomi
+
+var cognomi = ["Baglio","Storti","Poretti","Brazorf","Cecconi"];
+
+//Chiedere all'utente di inserire il suo cognome
+
+var cognome = prompt("INSERISCI IL TUO COGNOME( Iniziale maiuscola ): ");
+
+cognomi.push(cognome);
+
+console.log(cognomi);
+
+
+//Ordinare alfabeticamente e stampare
+
+cognomi.sort();
+
+document.getElementById("titolo").innerHTML= "Cognomi ordinati alfabeticamente: "
+document.getElementById("lista").innerHTML+="<li>"+ cognomi[0] +"</li>";
+document.getElementById("lista").innerHTML+="<li>"+ cognomi[1] +"</li>";
+document.getElementById("lista").innerHTML+="<li>"+ cognomi[2] +"</li>";
+document.getElementById("lista").innerHTML+="<li>"+ cognomi[3] +"</li>";
+document.getElementById("lista").innerHTML+="<li>"+ cognomi[4] +"</li>";
+document.getElementById("lista").innerHTML+="<li>"+ cognomi[5] +"</li>";
+
